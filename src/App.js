884ba5db0f4e5a58b2e8d42ffc7a2396295df3ee
Rafael Manrique <rafamanrique26@gmail.com>
2022-07-19 12:16:10 -0400

    Users Table Page done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
 import "./app.css"
 import Home from "./components/topbar/pages/home/Home";
+import UserList from "./components/topbar/pages/home/userList/UserList";
 import Sidebar from "./components/topbar/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
 
 function App() {
   return (
-    <div>
-      <Topbar/>
+    <BrowserRouter>
+      <Topbar />
       <div className="container">
-        <Sidebar/>
-        <Home/>
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/users" element={<UserList />}/>
+        </Routes>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
